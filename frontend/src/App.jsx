113dd29415166799cc4732724bcacde88d62d18c
MalcolmSmalls <smallsbeats@gmail.com>
2023-05-09 18:22:15 -0700

    Finish Modal logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import TaskList from './components/TaskList'
 import AddTaskModal from './components/AddTaskModal'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [openModal, setOpenModal] = useState(false)
 
   return (
     <main className='dark:bg-black bg-gray-800 w-full h-fit text-white font-Poppins'>
@@ -11,10 +11,13 @@ function App() {
         <h1 className='text-5xl uppercase tracking-widest font-bold'>
           Sets Todo List
         </h1>
-        <button className='bg-gray-900 text-gray-300 w-1/2 h-50 rounded-lg shadow-lg h-10 uppercase font-bold hover:bg-gray-950 hover:text-white'>
+        <button
+          onClick={() => setOpenModal(true)}
+          className='bg-gray-900 text-gray-300 w-1/2 h-50 rounded-lg shadow-lg h-10 uppercase font-bold hover:bg-gray-950 hover:text-white'
+        >
           <i className='fas fa-plus pr-2'></i>Add Task
         </button>
-        <AddTaskModal />
+        {openModal && <AddTaskModal closeModal={setOpenModal} />}
 
         <TaskList />
       </div>
--- frontend/src/components/AddTaskModal.jsx
+++ frontend/src/components/AddTaskModal.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function AddTaskModal() {
+export default function AddTaskModal({ closeModal }) {
   return (
-    <div className='backgroundBlur'>
-      <div className='modalContainer bg-gray-900 w-[800px] h-96 flex flex-col items-center rounded-lg gap-5 shadow-2xl'>
-        <button className='ml-auto pt-3 pr-4'>
-          <i className='fas fa-xmark text-3xl'></i>
-        </button>
+    <div className='backgroundBlur bg-black/50 w-[100vw] h-[100vh] fixed top-0 flex justify-center items-center backdrop-blur-md'>
+      <div className='modalContainer bg-gray-900 w-1/2 h-96 flex flex-col items-center rounded-lg gap-5 shadow-2xl'>
+        <div className='ml-auto pt-3 pr-4'>
+          <button onClick={() => closeModal(false)}>
+            <i className='fas fa-xmark text-3xl'></i>
+          </button>
+        </div>
         <div className='title'>
           <h1 className='uppercase text-xl'>Add A Task</h1>
         </div>
@@ -21,7 +23,10 @@ export default function AddTaskModal() {
           </div>
         </div>
         <div className='footer'>
-          <button className='bg-red-500 p-3 rounded-lg w-36 mr-5 hover:bg-red-600'>
+          <button
+            className='bg-red-500 p-3 rounded-lg w-36 mr-5 hover:bg-red-600'
+            onClick={() => closeModal(false)}
+          >
             Cancel
           </button>
           <button className='bg-green-500 p-3 rounded-lg w-36 hover:bg-green-600'>
