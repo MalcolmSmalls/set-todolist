f8184968439646672699817bac3b606354325f2b
MalcolmSmalls <smallsbeats@gmail.com>
2023-05-09 17:18:46 -0700

    Finish designing modal

--- frontend/src/components/AddTaskModal.jsx
+++ frontend/src/components/AddTaskModal.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 export default function AddTaskModal() {
   return (
     <div className='backgroundBlur'>
-      <div className='modalContainer bg-gray-900 w-[800px] h-96 flex flex-col items-center rounded-lg gap-5'>
+      <div className='modalContainer bg-gray-900 w-[800px] h-96 flex flex-col items-center rounded-lg gap-5 shadow-2xl'>
         <button className='ml-auto pt-3 pr-4'>
           <i className='fas fa-xmark text-3xl'></i>
         </button>
         <div className='title'>
           <h1 className='uppercase text-xl'>Add A Task</h1>
         </div>
-        <div className='body border-2 w-full pt-5'>
-          <input placeHolder='Task Name' />
-          <div>
-            <input placeHolder='Sets' />
-            <input placeHolder='Time per set' />
+        <div className='body pt-5 flex flex-col gap-5 w-1/2 items-center pb-5'>
+          <input placeHolder='Task Name' className='w-full rounded h-10 p-2' />
+          <div className='flex gap-5'>
+            <input placeHolder='Sets' className='w-1/2 rounded h-10 p-2' />
+            <input
+              placeHolder='Minutes per set'
+              className='w-1/2 rounded h-10 p-2'
+            />
           </div>
         </div>
         <div className='footer'>
