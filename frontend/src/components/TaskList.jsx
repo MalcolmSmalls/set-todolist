8ff1b3c5423a0d500c7267df3394fd580b436747
MalcolmSmalls <smallsbeats@gmail.com>
2023-05-20 12:07:58 -0700

    Add conditional if no tasks

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -35,50 +35,56 @@ export default function TaskList({ tasks, setTasks }) {
     })
   }
   return (
-    <div className='bg-gray-900 text-gray-300 w-1/2 h-50 rounded-lg shadow-lg h-fit'>
-      {tasks.map((item) =>
-        item.completed === false ? (
-          <div
-            key={item.id}
-            className='border-b border-b-gray-200/10 border-b-1'
-          >
-            <li className='list-none p-3 text-sm flex gap-4 items-center justify-center w-full'>
-              <div className='ml-5 rounded-full border-2 w-5 h-5'></div>
-              <div className='w-fit'>
-                {item.task}
-                <span className='text-indigo-500 text-xs'>
-                  {' '}
-                  x{' '}
-                  <span className='font-bold'>
-                    {item.sets} {item.sets > 1 ? 'sets' : 'set'}
-                    {item.time}
+    <div className='bg-gray-900 text-gray-300 w-1/2 h-50 rounded-lg shadow-lg h-fit mb-20'>
+      {tasks.length > 0 ? (
+        tasks.map((item) =>
+          item.completed === false ? (
+            <div
+              key={item.id}
+              className='border-b border-b-gray-200/10 border-b-1'
+            >
+              <li className='list-none p-3 text-sm flex gap-4 items-center justify-center w-full'>
+                <div className='ml-5 rounded-full border-2 w-5 h-5'></div>
+                <div className='w-fit'>
+                  {item.task}
+                  <span className='text-indigo-500 text-xs'>
+                    {' '}
+                    x{' '}
+                    <span className='font-bold'>
+                      {item.sets} {item.sets > 1 ? 'sets' : 'set'}
+                      {item.time}
+                    </span>
                   </span>
-                </span>
-              </div>
-              <div className='ml-auto w-1/3  flex justify-center'>
-                {item.time ? (
-                  <button
-                    className='uppercase border-[1px] p-2 rounded-lg text-xs hover:bg-indigo-500 
+                </div>
+                <div className='ml-auto w-1/3  flex justify-center'>
+                  {item.time ? (
+                    <button
+                      className='uppercase border-[1px] p-2 rounded-lg text-xs hover:bg-indigo-500 
                 hover:border-indigo-500 
                  hover:text-white '
-                  >
-                    Start Time
-                  </button>
-                ) : (
-                  <button
-                    id={item.id}
-                    className='uppercase border-[1px] p-2 rounded-lg text-xs hover:bg-indigo-500 
+                    >
+                      Start Time
+                    </button>
+                  ) : (
+                    <button
+                      id={item.id}
+                      className='uppercase border-[1px] p-2 rounded-lg text-xs hover:bg-indigo-500 
                   hover:border-indigo-500 
                    hover:text-white '
-                    onClick={(e) => minusSet(e.target.id)}
-                  >
-                    Minus Set
-                  </button>
-                )}
-              </div>
-            </li>
-          </div>
-        ) : null
+                      onClick={(e) => minusSet(e.target.id)}
+                    >
+                      Minus Set
+                    </button>
+                  )}
+                </div>
+              </li>
+            </div>
+          ) : null
+        )
+      ) : (
+        <div>
+          <button>Create a new Task</button>
+        </div>
       )}
     </div>
   )
