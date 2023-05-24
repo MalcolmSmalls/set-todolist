02dec12f73eda0c2d5840ef586cb5e54658f94fd
MalcolmSmalls <smallsbeats@gmail.com>
2023-05-23 18:31:09 -0700

    Add state to navbar to close and open sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false)
+
+  const showSidebar = () => {
+    setSidebar((prevSidebar) => !prevSidebar)
+  }
   return (
-    <div className='p-5 text-2xl'>
-      <Link to='#'>
-        <FaIcons.FaBars />
-      </Link>
-    </div>
+    <>
+      <div className='p-5 text-2xl'>
+        <Link to='#' onClick={showSidebar}>
+          <FaIcons.FaBars />
+        </Link>
+      </div>
+      <nav className={sidebar ? 'inline' : 'hidden'}>
+        <ul>
+          <Link to='#' onClick={showSidebar}>
+            <AiIcons.AiOutlineClose />
+          </Link>
+        </ul>
+      </nav>
+    </>
   )
 }
